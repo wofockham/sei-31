4902ae605ca15ea55c5c658d38b09a56e4bd6600
JT <certainthings@gmail.com>
2019-04-08 12:43:36 +1000

    gets and puts

---
+++ 05-ruby/greetings.rb
@@ -0,0 +1,16 @@
+# Greeting program to experiment with input and output
+
+print "What is your name? :"
+name = gets.chomp
+
+puts "Your name is #{ name }."
+
+print "What is your surname? :"
+surname = gets.chomp
+
+puts "Your full name is #{ name } #{ surname }."
+
+print "What is your address? :"
+address = gets.chomp
+
+puts "Your name is #{ name } #{ surname } and you live at #{ address }."
