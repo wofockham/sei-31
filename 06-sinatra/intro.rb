6f3bf48069c0017c164d125ac7931d22a75d27e5
JT <certainthings@gmail.com>
2019-04-10 16:05:27 +1000

    rubbish calculator

--- 06-sinatra/intro.rb
+++ 06-sinatra/intro.rb
@@ -13,7 +13,7 @@ get '/lucky_number' do
 end
 
 get '/uptime' do
-  "The server uptime is: #{ `uptime` }"
+  "The server uptime is: #{ `uptime` }</body></html>"
 end
 
 # Marx Brothers Fanclub
@@ -28,5 +28,15 @@ get '/fanclub/:first/:last' do
 end
 
 get '/fanclub/:first/:last/:favourite' do
-  "#{ params[:first].capitalize } #{ params[:last].upcase } is a proud member of the Marx Brothers fanclub. Their favourite Marx Brother is #{ params[:favourite].capitalize }."  
+  "#{ params[:first].capitalize } #{ params[:last].upcase } is a proud member of the Marx Brothers fanclub. Their favourite Marx Brother is #{ params[:favourite].capitalize }."
 end
+
+# Second crappiest calculator
+get '/multiply/:x/:y' do
+  result = params[:x].to_f * params[:y].to_f
+  "The result is <strong>#{ result }</strong>."
+end
+
+# Significant problems:
+# 1. How do we user input without making them change the URL?
+# 2. Where is the HTML?
