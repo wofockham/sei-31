fc2f5944488c909dc6559c47dcfc069fba45074b
JT <certainthings@gmail.com>
2019-04-11 14:56:31 +1000

    butterflies crud i have had enough

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -17,11 +17,38 @@ get '/butterflies/new' do
   erb :butterflies_new
 end
 
+# CREATE - Add a new butterfly to the database
+post '/butterflies' do
+  query = "INSERT INTO butterflies (name, family, image) VALUES ('#{params[:name]}', '#{params[:family]}', '#{params[:image]}')"
+  query_db query
+  redirect to('/butterflies') # GET request
+end
+
 # SHOW - Shows a single butterfly in more detail
 get '/butterflies/:id' do
   butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
   @butterfly = butterflies.first
   erb :butterflies_show
+end
+
+# EDIT - Shows a form to edit a single butterfly
+get '/butterflies/:id/edit' do
+  butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+  @butterfly = butterflies.first
+  erb :butterflies_edit
+end
+
+# UPDATE - Modify the database with new information for a particular butterfly
+post '/butterflies/:id' do
+  query = "UPDATE butterflies SET name='#{params[:name]}', family='#{params[:family]}', image='#{params[:image]}' WHERE id=#{params[:id]}"
+  query_db query
+  redirect to("/butterflies/#{ params[:id] }")
+end
+
+# DESTROY - Deletes a given a butterfly from the database
+get '/butterflies/:id/delete' do
+  query_db "DELETE FROM butterflies WHERE id=#{ params[:id] }"
+  redirect to("/butterflies")
 end
 
 def query_db(sql_statement)
