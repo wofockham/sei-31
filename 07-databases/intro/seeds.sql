7d11c1ae44afd3e5bc8e25ef94169818775d70ba
JT <certainthings@gmail.com>
2019-04-11 11:09:21 +1000

    SQL intro part 1

---
+++ 07-databases/intro/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO people (id, first_name, last_name, age) VALUES (1, 'John', 'Smith', 64);
+INSERT INTO people (id, first_name, last_name, age) VALUES (2, 'Jane', 'Smith', 67);
+INSERT INTO people VALUES (3, 'Little Bobby', 'Jones', 11);
