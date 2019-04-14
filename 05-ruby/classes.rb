641c297734da77f041afb0b2bc186c8c6aaa2f8c
JT <certainthings@gmail.com>
2019-04-15 09:18:12 +1000

    ruby inheritance example

--- 05-ruby/classes.rb
+++ 05-ruby/classes.rb
@@ -1,38 +1,41 @@
 require 'pry'
 
-class MarxBrother
-  # getter
-  def name()
-    @name # Instance variable
+# Superclass
+class Actor
+  def award_speech
+    "I would like to thank my parents and my agent. We did it baby"
   end
 
-  # setter
-  def name=(n)
+  def deny_allegations
+    "I deny that and I was drunk and I don't remember and he's not that attractive"
+  end
+end
+
+class Stooge < Actor
+  def terrible?
+    true
+  end
+end
+
+class MarxBrother < Actor
+  attr_accessor :name, :instrument, :vice
+
+  def initialize(n='', i='', v='hating fascism')
     @name = n
-  end
-
-  def instrument()
-    @instrument
-  end
-
-  def instrument=(i)
     @instrument = i
-  end
-
-  def vice()
-    @vice
-  end
-
-  def vice=(v)
     @vice = v
+  end
+
+  def perform
+    "My name is #{ @name } and I play the #{ @instrument }"
   end
 end
 
 
-groucho = MarxBrother.new
-groucho.name= 'Groucho Julius Marx'
-groucho.instrument= 'guitar'
-groucho.vice= 'cigars'
+groucho = MarxBrother.new 'Groucho Julius Marx', 'guitar', 'cigars'
+# groucho.name = 'Groucho Julius Marx'
+# groucho.instrument = 'guitar'
+# groucho.vice = 'cigars'
 
 harpo = MarxBrother.new
 harpo.name= 'Harpo Marx'
