19a4f7a70a848267bb142dade73057741ecb1f0a
JT <certainthings@gmail.com>
2019-04-16 11:19:40 +1000

    you can render automatically if the view has the same name as the action

--- 08-rails/view_helpers/app/controllers/pages_controller.rb
+++ 08-rails/view_helpers/app/controllers/pages_controller.rb
@@ -1,13 +1,11 @@
 class PagesController < ApplicationController
   def home
-    render :home
   end
 
   def numbers
     @amount = 242984.2323
     @large_number = 234423434349
     @phone = 2125551212
-    render :numbers
   end
 
   def text
@@ -15,14 +13,11 @@ class PagesController < ApplicationController
     @friend_count = 2
     @enemy_count = 2
     @story = "A very very very very very very very very very very very long time ago"
-    render :text
   end
 
   def assets
-    render :assets
   end
 
   def url
-    render :url
   end
 end
