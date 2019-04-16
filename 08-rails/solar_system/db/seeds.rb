e0c4ff007dae16e62994c86c2b244c274c9db64b
JT <certainthings@gmail.com>
2019-04-16 11:19:57 +1000

    solar system CRUD part i

---
+++ 08-rails/solar_system/db/seeds.rb
@@ -0,0 +1,5 @@
+Planet.destroy_all # Shades of Galactus
+
+Planet.create :name => 'Earth', :orbit => 24, :moons => 1
+Planet.create :name => 'Venus', :orbit => 17, :moons => 0
+Planet.create :name => 'Mars', :orbit => 33, :moons => 2
