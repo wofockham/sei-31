3b32015d0d40c484b7defca780da7391312223b8
JT <certainthings@gmail.com>
2019-04-29 12:58:36 +1000

    tdd bank finish

--- 09-tdd/bank/bank.rb
+++ 09-tdd/bank/bank.rb
@@ -9,4 +9,16 @@ class Bank
   def create_account(account_name, balance)
     @accounts[account_name] = balance
   end
+
+  def deposit(account_name, amount)
+    @accounts[account_name] += amount
+  end
+
+  def withdraw(account_name, amount)
+    @accounts[account_name] -= amount if (amount <= @accounts[account_name])
+  end
+
+  def balance(account_name)
+    @accounts[account_name]
+  end
 end
--- 09-tdd/bank/spec/bank_spec.rb
+++ 09-tdd/bank/spec/bank_spec.rb
@@ -1,23 +1,50 @@
 require_relative '../bank'
 
 describe Bank do
+  let(:bank) { Bank.new 'TDD Bank' }
   describe '.new' do
     it 'create a new bank object' do
-      bank = Bank.new 'TDD Bank'
       expect(bank).to_not be nil
     end
 
     it 'assigns a bank a name' do
-      bank = Bank.new 'TDD Bank'
       expect(bank.name).to eq 'TDD Bank'
     end
   end
 
   describe '#create_account' do
     it 'creates an account for a particular user' do
-      bank = Bank.new 'TDD Bank'
       bank.create_account 'Craigsy', 7
       expect(bank.accounts['Craigsy']).to eq 7
+    end
+  end
+
+  describe '#deposit' do
+    it 'deposits an amount into a particular account' do
+      bank.create_account 'Jonesy', 200
+      bank.deposit 'Jonesy', 300
+      expect(bank.accounts['Jonesy']).to eq 500
+    end
+  end
+
+  describe '#withdraw' do
+    it 'withdraws an amount from a particular account' do
+      bank.create_account 'Bazza', 200
+      bank.withdraw 'Bazza', 50
+      expect(bank.accounts['Bazza']).to eq 200-50
+    end
+
+    it 'ignores withdrawals that exceed the balance' do
+      bank.create_account 'Daisy', 1
+      bank.withdraw 'Daisy', 1_000_000
+      expect(bank.balance('Daisy')).to eq 1
+    end
+  end
+
+  describe '#balance' do
+    it 'returns the balance for some particular account' do
+      bank.create_account 'Mad Robby', 500
+      expect(bank.balance('Mad Robby')).to eq 500
     end
   end
 end
