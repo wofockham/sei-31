700e3d8b57c727768295165c827bfb2992a807c6
JT <certainthings@gmail.com>
2019-05-03 11:21:55 +1000

    hello react

---
+++ 12-react/react-intro/src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  // Every React component must have a method called render()
+  render() {
+    return (
+      <h1 class="hey-there">Hello World!!!!!!!!!! OMG LIVE RELOADING</h1>
+    );
+  }
+}
+
+export default HelloWorld;
