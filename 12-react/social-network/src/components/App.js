23adbbf2f09c6f02f2b634c6213c3decd4e5fc75
JT <certainthings@gmail.com>
2019-05-03 13:01:25 +1000

    Social Network skellington

---
+++ 12-react/social-network/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Profile from './Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile name="Groucho" age="101" motto="No gods no master" pic="http://fillmurray.com/400/400" />
+      <Profile name="Harpo" age="98" motto="" pic="http://fillmurray.com/401/400" />
+      <Profile name="Chico" age="99" motto="No gods no master" pic="http://fillmurray.com/400/401" />
+    </div>
+  );
+}
+
+export default App;
---
+++ 12-react/social-network/src/components/Profile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return (
+      <h2>{ this.props.name }'s profile coming soon</h2>
+    );
+  }
+}
+
+export default Profile;
