78772afe3f346da4dc03688a764bc188ceefa5c4
JT <certainthings@gmail.com>
2019-05-07 11:15:14 +1000

    fast recursive fibonacci examples

--- 13-advanced/recursion/fibonacci.rb
+++ 13-advanced/recursion/fibonacci.rb
@@ -14,10 +14,37 @@ def fibonacci_recursive(n)
   if n == 1 || n == 2
     1 # base case
   else
-    # Quadratic O(N^2)
+    # Exponential O(2^N)
     fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
   end
 end
+
+# Memoisation
+def fib_memo(n)
+  @fib ||= {} # @fib = @fib || {}
+
+  if @fib[n] # Previously found and saved in this "memoisation" hash
+    @fib[n]
+  elsif n == 1 || n == 2
+    1 # base case
+  else
+    result = fib_memo(n - 1) + fib_memo(n - 2)
+    @fib[n] = result
+    result
+  end
+end
+
+# Linear recursion
+# TODO: Research Tail Call Optimisation (TCO)
+def fib(n, a=1, b=1)
+  if n == 1 || n == 2 # Base case
+    b
+  else
+    # Linear: O(N)
+    fib(n-1, b, a+b) # Tail recursion
+  end
+end
+
 
 require 'pry'
 binding.pry
