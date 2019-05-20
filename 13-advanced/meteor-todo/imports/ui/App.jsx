8dc545f83eec1bede6bedbc2edb022bebecee500
JT <certainthings@gmail.com>
2019-05-20 15:03:39 +1000

    meteor todos

--- 13-advanced/meteor-todo/imports/ui/App.jsx
+++ 13-advanced/meteor-todo/imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks';
@@ -6,8 +7,41 @@ import { Tasks } from '../api/tasks';
 import Task from './Task';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hideCompleted: false
+    };
+  }
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim()
+
+    Tasks.insert({
+      text, // equivalent to: text: text,
+      createdAt: new Date()
+    });
+
+    // Mutation AND touching the DOM: very naughty indeed
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.complete);
+    }
+
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -16,7 +50,25 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideComplete}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide completed tasks
+          </label>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type here to add new tasks"
+            />
+          </form>
         </header>
 
         <ul>
@@ -29,6 +81,7 @@ class App extends Component {
 
 export default withTracker(() => {
   return {
-    tasks: Tasks.find({}).fetch()
+    tasks: Tasks.find({}, {sort: { createdAt: -1 } }).fetch(), // descending order of createdAt
+    incompleteCount: Tasks.find({ complete: {$ne: true} }).count()
   }
 })(App);
--- 13-advanced/meteor-todo/imports/ui/Task.jsx
+++ 13-advanced/meteor-todo/imports/ui/Task.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
+import { Tasks } from '../api/tasks';
+
 export default class Task extends Component {
+  toggleComplete() {
+    Tasks.update(this.props.task._id, {
+      $set: { complete: !this.props.task.complete }
+    });
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
+    const taskClassName = this.props.task.complete ? 'checked' : '';
     return (
-      <li>{this.props.task.text}</li>
+      <li className={ taskClassName }>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+        <input
+          type="checkbox"
+          readOnly
+          checked={!! this.props.task.complete}
+          onClick={this.toggleComplete.bind(this)}
+        />
+        <span className="text">
+          {this.props.task.text}
+        </span>
+      </li>
     );
   }
 }
