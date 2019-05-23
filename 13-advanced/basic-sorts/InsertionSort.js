250cbe87e67cbdc5d55fc1c9e4d3e2de67f875ad
JT <certainthings@gmail.com>
2019-05-23 11:18:08 +1000

    insertion sort

--- 13-advanced/basic-sorts/InsertionSort.js
+++ 13-advanced/basic-sorts/InsertionSort.js
@@ -1,19 +1,15 @@
 function insertionSort (items) {
-  // Loop through each element
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
 
-  // store the current item value so it can be placed correctly
-  // in the sorted portion of the array
+    for (var j = i-1; j >= 0 && items[j] > item; j--) {
+      items[j + 1] = items[j];
+    }
 
-  // Loop backward through the sorted portion of the array
-  // and scoot everything over until you find the right place to
-  // insert the value you're working with
+    items[j + 1] = item;
 
-  // Copy each item to the next slot over, as long as the value is smaller
-  // than the item in the sorted array we're looking at (items[j] > value)
+  }
 
-  // We can now insert the item in its sorted location
-
-  // Remember to return the list!
   return items;
 }
 
