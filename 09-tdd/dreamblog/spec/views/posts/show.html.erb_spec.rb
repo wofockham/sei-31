f625740c78a39c04f4bafd44a39aeab96bd549c1
JT <certainthings@gmail.com>
2019-05-28 15:59:25 +1000

    example of more rspec tests via scaffolding

---
+++ 09-tdd/dreamblog/spec/views/posts/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "posts/show", type: :view do
+  before(:each) do
+    @post = assign(:post, Post.create!(
+      :title => "Title",
+      :author => "Author",
+      :content => "MyText"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Title/)
+    expect(rendered).to match(/Author/)
+    expect(rendered).to match(/MyText/)
+  end
+end
