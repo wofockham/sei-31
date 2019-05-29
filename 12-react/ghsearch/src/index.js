73135f5c2d8a510053fb30d6cb5afb11d2bd02e4
JT <certainthings@gmail.com>
2019-05-29 12:08:46 +1000

    DIY create-react-app

---
+++ 12-react/ghsearch/src/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'; // Aha! This is really Preact. IN YOUR FACE, FACEBOOK
+import ReactDOM from 'react-dom';
+
+import 'preact/devtools';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.body);
---
+++ 12-react/ghsearch/webpack.config.js
@@ -0,0 +1,35 @@
+let path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?/i,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [
+            ['transform-react-jsx']
+          ]
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    compress: true,
+    historyApiFallback: true
+  }
+};
