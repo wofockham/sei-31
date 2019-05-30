84d83a56bd3eaa5ab4112fdeefddab49d2e12515
JT <certainthings@gmail.com>
2019-05-30 12:05:48 +1000

    c and assembly demo

---
+++ 13-advanced/assembly/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Comments */
+
+int add (int a, int b) {
+  return a + b;
+}
+
+int main () {
+  int result = add(7, 25);
+  printf("The result is %d\n", result);
+  printf("Hello world\n");
+  return 0; // Everything is okay.
+}
---
+++ 13-advanced/assembly/hello.s
@@ -0,0 +1,68 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 11
+	.globl	_add
+	.align	4, 0x90
+_add:                                   ## @add
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp0:
+	.cfi_def_cfa_offset 16
+Ltmp1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp2:
+	.cfi_def_cfa_register %rbp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %esi
+	addl	-8(%rbp), %esi
+	movl	%esi, %eax
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+	.globl	_main
+	.align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp3:
+	.cfi_def_cfa_offset 16
+Ltmp4:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp5:
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$7, %edi
+	movl	$25, %esi
+	movl	$0, -4(%rbp)
+	callq	_add
+	leaq	L_.str(%rip), %rdi
+	movl	%eax, -8(%rbp)
+	movl	-8(%rbp), %esi
+	movb	$0, %al
+	callq	_printf
+	leaq	L_.str.1(%rip), %rdi
+	movl	%eax, -12(%rbp)         ## 4-byte Spill
+	movb	$0, %al
+	callq	_printf
+	xorl	%esi, %esi
+	movl	%eax, -16(%rbp)         ## 4-byte Spill
+	movl	%esi, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"The result is %d\n"
+
+L_.str.1:                               ## @.str.1
+	.asciz	"Hello world\n"
+
+
+.subsections_via_symbols
