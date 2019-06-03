d7125949c0b3ec87e2f3fa8feca3bd43521876d4
JT <certainthings@gmail.com>
2019-06-03 12:32:38 +1000

    bill murray gatsby fansite

---
+++ 12-react/gatsby/graphql/gatsby-config.js
@@ -0,0 +1,17 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography'
+      }
+    }
+  ]
+}
---
+++ 12-react/gatsby/graphql/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <h1>The Wonderful World of Bill Murray</h1>
+    <div>
+      <img src="http://fillmurray.com/400/700" alt="Bill Murray"/>
+    </div>
+  </Layout>
+);
